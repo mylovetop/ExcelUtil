39c5f48d4bb00b93b61458d5897b5413a9a5a502
默书 <304580317@qq.com>
2019-06-04 11:33:00 +0800

    下载

--- src/test/java/com/sargeraswang/util/ExcelUtil/TestDownload.java
+++ src/test/java/com/sargeraswang/util/ExcelUtil/TestDownload.java
@@ -1,4 +1,29 @@
-@RequestMapping(value = "excel", method = RequestMethod.GET)
+import com.seakoon.common.utils.excel.ExcelUtil;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.Row;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.*;
+
+@Controller
+@RequestMapping("/export")
+@Api(value = "Api - ExportController", description = "导出")
+public class ExportController {
+    
+    @RequestMapping(value = "excel", method = RequestMethod.GET)
     public void excel(HttpServletResponse response) throws IOException {
 
         String fileName= System.currentTimeMillis()+".xls";
@@ -43,3 +68,4 @@
 
 
     }
+}
